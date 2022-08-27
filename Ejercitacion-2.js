973dc7ed70628667e99a7356353ad211cee7d730
Marcos Anton <marquitosloko@gmail.com>
2022-08-27 18:34:56 -0300

    Cambios en la ejercitaciones

---
+++ Ejercitacion-2.js
@@ -0,0 +1,9 @@
+const mipersona = ["Marcos Anton", 22, true, new Date('1999-10-27')];
+const libroFav = {
+    titulo: "Los androides sueñan con ovejas electricas",
+    autor: "Philip K. Dick",
+    fecha: new Date('1968'),
+    url: "https://www.philosophia.cl/biblioteca/dick/runner.pdf"
+}
+console.log(libroFav)
+console.log(mipersona);
